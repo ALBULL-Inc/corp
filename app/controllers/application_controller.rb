7847389596b6bcce1488405c3c18bf01326c280a
Akihiro Okuda <axio@o9da.net>
2016-05-28 13:26:00 +0900

    sp用画面 ・共通系とトップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  include Jpmobile::ViewSelector
 
   before_action :constant
   before_action :add_breadcrumb_of_root
+
+  protect_from_forgery with: :exception
 
   private
     def constant
