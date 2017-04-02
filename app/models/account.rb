282dab8b0681ccfcb7a16018a9fb5efbb23db063
Akihiro Okuda <axio@o9da.net>
2017-04-02 20:45:23 +0900

    lineアカウントとのヒモ付

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,6 +9,7 @@ class Account < ApplicationRecord
   delegate :firstname, :lastname, :nickname, :gender, :blood, :birthday, to: :profile
   has_many :oauths, class_name: 'AccountOAuth', dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_one :line_oauth, -> { where(provider: 'line') }, class_name: 'AccountOAuth'
 
   # Scope
   default_scope -> { eager_load(:profile) }
