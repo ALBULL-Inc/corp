157176d569a631ab1ca75579e455849bed3aa0ea
Akihiro Okuda <axio@o9da.net>
2017-07-18 11:33:42 +0900

    sitemapをファイルのsymlinkへ

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :repo_url, 'git@github.com:your-corp/corp.git'
 set :rbenv_ruby, File.read('.ruby-version').strip
 
 #set :linked_files, %W{.env config/database.yml config/settings/#{fetch :stage}.yml config/settings/slack_notifier.yml}
-set :linked_files, %W{config/database.yml config/settings/slack.yml config/settings/#{fetch :stage}.yml .env}
-set :linked_dirs, %w{log public/sitemaps tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :linked_files, %W{config/database.yml config/settings/slack.yml config/settings/#{fetch :stage}.yml public/sitemap.xml.gz .env}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 set :keep_releases, 5
 
