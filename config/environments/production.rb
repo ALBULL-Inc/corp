bd8c2cff4173a6c5f499e4eeef90cdc59ba5f3fd
Akihiro Okuda <axio@o9da.net>
2017-07-18 12:52:31 +0900

    微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.logger = ActiveSupport::Logger.new("#{Rails.root}/log/production.log", 'daily')
+  config.logger = ActiveSupport::Logger.new(Rails.root.join('log','production.log'), 'daily')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
