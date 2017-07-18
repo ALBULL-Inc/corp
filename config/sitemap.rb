94b588a260ec37dd5e5549ef071243901b60036e
Akihiro Okuda <axio@o9da.net>
2017-07-18 11:55:10 +0900

    sitemap修正

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,12 +5,22 @@ SitemapGenerator::Sitemap.sitemaps_path = ''
 SitemapGenerator::Sitemap.create do
   # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
-  add about_us_path, priority: 0.7
-  add recruits_path, priority: 0.7
-  add new_inquiry_path, priority: 0.7
-  add new_account_session_path, priority: 0.7
+  add about_us_path,            priority: 0.7, changefreq: 'daily'
+  add mission_path,             priority: 0.7, changefreq: 'daily'
+  add policy_childcare_path,    priority: 0.7, changefreq: 'daily'
 
-  add mission_path, priority: 0.5
-  add policy_childcare_path, priority: 0.5
-  add places_path, priority: 0.5
+  #add topics_path,              priority: 0.7, changefreq: 'daily'
+  Topic.enables.find_each do |topic|
+    add topic_path(topic),      priority: 0.5, changefreq: 'daily', lastmod: topic.publish_at
+  end
+
+  add recruits_path,            priority: 0.7, changefreq: 'daily'
+  Recruit.enables.find_each do |recruit|
+    add recruit_path(recruit),  priority: 0.5, changefreq: 'daily', lastmod: recruit.updated_at
+  end
+
+  add places_path,              priority: 0.7, changefreq: 'daily'
+  Place.find_each do |place|
+    add place_path(place),      priority: 0.5, changefreq: 'daily', lastmod: place.updated_at
+  end
 end
