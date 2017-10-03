dd539ae307a8d0b1576203c377bca491c9096dbf
Akihiro Okuda <axio@o9da.net>
2017-10-03 17:16:28 +0900

    fix

--- db/migrate/20171003075844_add_columns_to_places.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPlaces < ActiveRecord::Migration[5.0]
-  def change
-    add_column :places, :enable, :boolean, default: false, null: false
-    add_column :places, :position, :integer, default: 99999, null: false
-  end
-end
