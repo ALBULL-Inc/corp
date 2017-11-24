7d56b69f36314200f4b779972c1eeaa3cac5028a
Akihiro Okuda <axio@o9da.net>
2017-11-25 00:10:53 +0900

    お問い合わせページにカテゴリを追加

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,10 +3,12 @@ class InquiriesController < ApplicationController
   def new
     add_breadcrumb "お問合せ", new_inquiry_url
     @inquiry = Inquiry.new
+    @concerns = Inquiry.concerns.map{|k,v| [v,k]}
   end
 
   def create
     @inquiry = Inquiry.new(inquiry_params)
+    @concerns = Inquiry.concerns.map{|k,v| [v,k]}
     unless @inquiry.valid?
       render :new and return
     end
@@ -16,6 +18,6 @@ class InquiriesController < ApplicationController
 
   private
     def inquiry_params
-      params.require(:inquiry).permit(:email, :name, :title, :body)
+      params.require(:inquiry).permit(:concern, :email, :name, :title, :body)
     end
 end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,7 @@
 class Inquiry
   include ActiveModel::Model
 
-  attr_accessor :email, :name, :title, :body
+  attr_accessor :email, :name, :title, :body, :concern
 
   validates :email,:name,:body, presence: true
 
@@ -11,11 +11,25 @@ class Inquiry
     end
     notifier = Slack::Notifier.new Settings.slack.webhook_url.inquiry, username: "pippi"
     inquery_payload = {
-      title: "#{self.name}(#{self.email})さんからお問い合わせです",
+      title: "#{self.name}(#{self.email})さんから#{self.concern_str}のお問い合わせです",
       title_link: "mailto:#{self.email}",
       text: self.body.force_encoding('utf-8')
     }
     notifier.ping ":thought_balloon: お問い合わせを頂きました:exclamation:", attachments: [inquery_payload]
+  end
+
+  def concern_str
+    Inquiry.concerns[self.concern]
+  end
+
+  def self.concerns
+    h = []
+    defaults = [["1", "採用について"], ["2", "取材について"]]
+    places = Place.enables.pluck(:id,:name).map{|id,name| ["p#{id}", "#{name}について"]}
+    h += defaults
+    h += places
+    h << ["99", "その他"]
+    h.to_h
   end
 
 end
