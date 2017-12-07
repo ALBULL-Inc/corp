c1fd1abcb230f25a55a07d0348d5762bc15d5369
Akihiro Okuda <axio@o9da.net>
2017-12-07 16:09:01 +0900

    photoレコード作成バッチ作成

---
+++ batch/insert_photos.rb
@@ -0,0 +1,24 @@
+# rails runner batch/insert_photos.rb :施設名: :YEARMONTH: :フォルダ:
+
+
+place_key = ARGV[0]
+place = Place.find_by_key(place_key)
+print "place: #{place.name}", "\n"
+ym = ARGV[1]
+month = Month.find_or_create_by(ym: ym)
+print "month: #{month.ym}", "\n"
+folder_path = ARGV[2]
+files = Dir.open(folder_path)
+
+
+files.each do |file|
+  if file == "." or file == ".."
+    next
+  end
+  f = File.open("#{files.path}/#{file}")
+  photo = Photo.new(image: f)
+  if photo.save
+    place.photos << photo
+    month.photos << photo
+  end
+end
