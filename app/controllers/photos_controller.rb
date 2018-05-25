160eed5c7f175bfdc5ae2acb8d24dd27573c5dda
Akihiro Okuda <axio@o9da.net>
2018-05-25 20:58:55 +0900

    bug fix

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,8 +7,8 @@ class PhotosController < ApplicationController
     redirect_to @place and return unless @place.allow?(@current_family)
     target = @current_family.usage_months.where(usage_records: {place: @place}).includes(:photos)
     if params[:ym].present?
-      month = Month.find_by(ym: params[:ym])
-      @month = @current_family.usage_records.by_place(@place).by_month(month)
+      @month = Month.find_by(ym: params[:ym])
+      redirect_to @place and return unless @current_family.usage_records.by_place(@place).by_month(@month).exists?
       @photos = @place.photos.joins(:month).where(months: {ym: @month.ym}).page(params[:page]).per(15)
     else
       @months = Month.where(id: @current_family.usage_records.by_place(@place).pluck(:month_id)).page(params[:page]).per(9)
