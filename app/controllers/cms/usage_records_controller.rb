3fa2aa18bd2b757807c88ecd34d9665f307cf602
Akihiro Okuda <axio@o9da.net>
2018-06-09 13:58:24 +0900

    利用登録の園児一覧に所属を表示

--- app/controllers/cms/usage_records_controller.rb
+++ app/controllers/cms/usage_records_controller.rb
@@ -20,7 +20,7 @@ class Cms::UsageRecordsController < Cms::ApplicationController
   # GET /cms/usage_records/new
   def new
     @usage_record = UsageRecord.new
-    @children = Child.all
+    @children = Child.order(:place_id, :created_at)
   end
 
   # GET /cms/usage_records/1/edit
