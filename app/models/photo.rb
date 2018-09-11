054ae54e9943259f3a8ed924c4af610288493c05
Akihiro Okuda <axio@o9da.net>
2018-09-11 09:30:17 +0900

    写真向き

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,10 @@
 class Photo < ApplicationRecord
-  dragonfly_accessor :image
+  dragonfly_accessor :image do
+    after_assign do |attachment|
+      # Auto orient all the images - so they will look as they should
+      attachment.convert! '-auto-orient'
+    end
+  end
 
   has_one :months_photo
   has_one :month, through: :months_photo
