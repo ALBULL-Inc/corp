1e10dc5eebec8cd5286ee971073a2a43b5cc38d6
Akihiro Okuda <axio@o9da.net>
2019-02-26 17:24:28 +0900

    description修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,7 +9,7 @@ class Blog < ApplicationRecord
   def body_md
     return @body_md if @body_md
     renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
+    markdown = Redcarpet::Markdown.new(renderer, hard_wrap: true, autolink: true, tables: true)
     @body_md = markdown.render(self.body)
   end
 
